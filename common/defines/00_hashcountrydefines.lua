c74c33766ff393417f960d2a8a6f346e5c834308
Pieman90 <71159224+Pieman90@users.noreply.github.com>
2024-02-01 22:11:26 -0500

    Old mod country QOL

--- common/defines/00_hashcountrydefines.lua
+++ common/defines/00_hashcountrydefines.lua
@@ -1,2 +1,16 @@
 --Country overrides
 --NDefines.NCountry.
+NDefines.NCountry.MONARCH_MIN_SKILL = 1
+NDefines.NCountry.ABDICATE_AGE_THRESHOLD = 50
+NDefines.NCountry.ABDICATE_RULING_LENGTH_THRESHOLD = 15
+NDefines.NCountry.MAX_IDEA_GROUPS_FROM_SAME_CATEGORY = 0.4
+NDefines.NCountry.MINIMUM_POLICY_TIME = 5
+NDefines.NCountry.PS_REPLACE_RIVAL = 50
+NDefines.NCountry.DEBT_LIMIT_ADM = -999
+NDefines.NCountry.DEBT_LIMIT_MIL = -999
+--to consider
+--REVOLT_SIZE_DEVELOPMENT_MULTIPLIER = 0.3,		-- Multiplied with the province's development
+--REVOLT_SIZE_BASE = 5,
+--REVOLT_TECH_IMPACT = 0.04, 			-- % each tech increases size of rebels by this percent.
+--REVOLT_TECH_MORALE = 0.015,
+--REBEL_ARTILLERY_INCREASE_LEVEL_2_SIZE = 0.25,
