abcd45c0c4071269244bf3aee14c6e9adf2cf8e1
Pieman90 <71159224+Pieman90@users.noreply.github.com>
2024-02-01 22:17:40 -0500

    add mil defines

--- common/defines/00_hashmilitarydefines.lua
+++ common/defines/00_hashmilitarydefines.lua
@@ -1,2 +1,10 @@
 --Military overrides
 --NDefines.NMilitary.
+NDefines.NMilitary.NOMAD_NON_PLAINS_SHOCK_PENALTY = -0.0
+NDefines.NMilitary.DEFAULT_WARGOAL_TICKINGWARSCORE_BONUS = 0.8		-- Amount of warscore per month since attacker/defender started getting the bonus for completing war goal
+NDefines.NMilitary.WARGOAL_MAX_BONUS = 40
+NDefines.NMilitary.DEFAULT_WARGOAL_WARSCORE_BONUS	= 6			-- Warscore bonus
+NDefines.NMilitary.DEFAULT_WARGOAL_BATTLESCORE_BONUS = 4		-- Battle score bonus from winning battles
+NDefines.NMilitary.SUPERIORITY_WARGOAL_WARSCORE_THRESHOLD	= 20	-- Needed battle score for getting ticking war score for war goal superiority
+NDefines.NMilitary.WARSCORE_MAX_FROM_BATTLES = 50					-- maximum amount to get from a battles .
+NDefines.NMilitary.SUPPLY_DEPOT_MIL_COST = 10
