e76f6ca733769644952c66cf2873a622d60b1628
Xaimelee <48264234+Xaimelee@users.noreply.github.com>
2024-02-12 21:18:51 +1000

    Fixed trade propagate divider

--- common/defines/00_hasheconomydefines.lua
+++ common/defines/00_hasheconomydefines.lua
@@ -1,5 +1,5 @@
 --Military overrides
 --NDefines.NEconomy.
 NDefines.NEconomy.LAND_TECH_MERC_MAINTENANCE_IMPACT = 0.03
-NDefines.NEconomy.TRADE_PROPAGATE_DIVIDER = 0.08
+NDefines.NEconomy.TRADE_PROPAGATE_DIVIDER = 8
 NDefines.NEconomy.TRADE_NON_CAPITAL_OFFICE = -0.25
