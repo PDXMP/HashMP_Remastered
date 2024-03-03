66cbc90615a98dbf31a92703d906a9e986502cca
TornadoWatch <superbrawlsmasher@gmail.com>
2024-03-03 18:26:16 -0500

    Tradition gained from naval combat increased by 50%. (It's really, really low in vanilla for most countries.)

--- common/defines/00_hashmilitarydefines.lua
+++ common/defines/00_hashmilitarydefines.lua
@@ -41,3 +41,4 @@ NDefines.NMilitary.HEAVY_SHIP_COST = 35 							-- _MDEF_HEAVY_SHIP_COST = 10,
 NDefines.NMilitary.LIGHT_SHIP_COST = 20 							-- _MDEF_LIGHT_SHIP_COST = 10,
 NDefines.NMilitary.GALLEY_COST = 10								-- _MDEF_GALLEY_COST = 10,
 NDefines.NMilitary.TRANSPORT_COST = 5							-- _MDEF_TRANSPORT_COST = 10,
+NDefines.NMilitary.TRADITION_GAIN_NAVAL = 60						-- Tradition gain base value from naval combat.
