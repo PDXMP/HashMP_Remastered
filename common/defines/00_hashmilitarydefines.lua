2ca35a13ea2f1cc69fe48b48fc0dd5499da48cb6
Xaimelee <48264234+Xaimelee@users.noreply.github.com>
2024-03-11 21:27:51 +1300

    Increased cav suppression

--- common/defines/00_hashmilitarydefines.lua
+++ common/defines/00_hashmilitarydefines.lua
@@ -43,3 +43,4 @@ NDefines.NMilitary.GALLEY_COST = 10								-- _MDEF_GALLEY_COST = 10,
 NDefines.NMilitary.TRANSPORT_COST = 5							-- _MDEF_TRANSPORT_COST = 10,
 NDefines.NMilitary.TRADITION_GAIN_NAVAL = 60						-- Tradition gain base value from naval combat.
 NDefines.NMilitary.CAVALRY_COST = 20
+NDefines.NMilitary.CAV_SUPPRESSION = 0.5
